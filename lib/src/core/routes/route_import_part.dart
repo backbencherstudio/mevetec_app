c2dc07604d2e61e72e2dfef766355ddb9dcab906
nahidulislamshakin <shakinhabib2000@gmail.com>
2025-07-01 17:03:10 +0600

    splash screen added

--- lib/src/core/routes/route_import_part.dart
+++ lib/src/core/routes/route_import_part.dart
@@ -1,4 +1,8 @@
 
 import 'package:go_router/go_router.dart';
+import 'package:mevetec_app/src/core/routes/route_name.dart';
+
+import '../../features/screens/splash/presentation/splash_screen.dart';
+import 'build_page_with_transition.dart';
 
 part 'route_config.dart';
